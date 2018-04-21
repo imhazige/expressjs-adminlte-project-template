077dc54f519caddd38cd086a69c77fda2a9b3aba
MicroWin10-1724\Administrator <asdba@adasd.com>
2018-04-21 16:33:47 +0800

    test remove git commit hooks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,7 +21,8 @@ class Footer extends React.Component {
         <strong>
           Copyright &copy; 2014-2016{' '}
           <a href="https://adminlte.io">Almsaeed Studio</a>.
-        </strong>{' '}
+        </strong>
+        {' - '}
         All rights reserved.
       </footer>
     );
